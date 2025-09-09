a580a0505815f4ae89eb6a732c0499207c2ef7fb
Mohamed82 <mabdallah97643@gmail.com>
2025-09-10 01:10:44 +0300

    Task3 Version Control With GitGithub

--- IoT Sensor Data Logger/Day 5/sensor_script.py
+++
@@ -1,9 +0,0 @@
-import os, time, random
-
-# Get sensor type from environment variable
-sensor = os.getenv("SENSOR_TYPE", "unknown")
-
-while True:
-    value = random.randint(15, 45)
-    print(f"{time.ctime()} | {sensor}: {value}")
-    time.sleep(2)
